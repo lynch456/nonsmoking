157751ded0d320b825d1201f1734520043812671
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2022-06-05 15:33:57 +0900

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const currentDate = document.getElementById("date");
 const menuToggle = document.getElementById("menu-bar");
 let newDate = new Date();
-let date = [newDate.getFullYear(), newDate.getDay(), newDate.getDate()];
+let date = [newDate.getFullYear(), newDate.getMonth() + 1, newDate.getDate()];
 
 currentDate.textContent = `${date[0]}년 ${date[1]}월 ${date[2]}일`;
 
@@ -26,19 +26,48 @@ timerSec.textContent = "00초";
 function timerStart() {
   sec++;
   timerSec.textContent = sec > 9 ? sec + "초" : "0" + sec + "초";
+  if (sec > 59) {
+    min++;
+    timerMin.textContent = min > 9 ? min + "분" : "0" + min + "분";
+    sec = 0;
+    timerSec.textContent = "00초";
+  } else if (min > 59) {
+    hour++;
+    timerHour.textContent = hour > 9 ? hour + "시간" : "0" + hour + "시간";
+    min = 0;
+    timerMin.textContent = "00분";
+  } else if (hour >= 24 && hour % 24 === 0) {
+    day + 1;
+    day > 9 ? day + "일" : "0" + day + "일";
+    hour = 0;
+    timerHour.textContent = "00시간";
+  }
 }
 
-const button = document.getElementById("start-btn");
-button.textContent = "금연시작";
-let btnToggle = "start";
-button.addEventListener("click", () => {
-  if (btnToggle === "start") {
-    timeId = setInterval(timerStart, 1000);
-    button.textContent = "금연중단";
-    btnToggle = "end";
-  } else if (btnToggle === "end") {
-    clearInterval(timeId);
-    button.textContent = "금연시작";
-    btnToggle = "start";
-  }
+const numberCount = document.getElementById("number");
+const moneyCount = document.getElementById("money");
+
+numberCount.textContent = "0개비";
+moneyCount.textContent = "0원";
+let money = 0;
+let number = 0;
+let moneyId;
+let numberId;
+function moneyCounting() {
+  money += 4500 / 86400;
+  moneyCount.textContent = `${money.toFixed(1)}원`;
+}
+
+function numberCounting() {
+  number += 20 / 86400;
+  numberCount.textContent = `${number.toFixed(1)}개비`;
+}
+const endButton = document.getElementById("end-btn");
+
+endButton.textContent = "금연종료";
+
+endButton.addEventListener("click", () => {
+  clearInterval(timeId);
+  clearInterval(moneyId);
+  clearInterval(numberId);
 });
--- userInfo.js
+++ userInfo.js
@@ -1,8 +1,18 @@
+const inputInfo = document.getElementById("info-input");
+const userInfo = document.getElementById("user-info");
 const chooseDate = document.getElementById("current-date");
 const chooseTime = document.getElementById("current-time");
+const chooseDateValue = document.getElementById("dateValue");
+const startButton = document.getElementById("start-btn");
 
-let toDay = new Date();
-let newDay = new Date(toDay);
+startButton.textContent = "금연시작";
 
-chooseDate.innerHTML = newDay;
-chooseTime.innerHTML = "";
+inputInfo.addEventListener("click", () => {
+  userInfo.classList.remove("hide");
+});
+startButton.addEventListener("click", () => {
+  timeId = setInterval(timerStart, 10);
+  moneyId = setInterval(moneyCounting, 10);
+  numberId = setInterval(numberCounting, 10);
+  userInfo.classList.add("hide");
+});
